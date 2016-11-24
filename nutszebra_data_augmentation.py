9872f2b26ca44954d8b7cc48125c8115e3a7b513
nutszebra <nutszebra@gmail.com>
2016-11-24 09:48:56 +0900

    updated

--- nutszebra_data_augmentation.py
+++ nutszebra_data_augmentation.py
@@ -41,6 +41,19 @@ class DataAugmentationCifar10NormalizeBig(object):
         return da.x, da.info
 
 
+class DataAugmentationCifar10NormalizeHuge(object):
+
+    @staticmethod
+    def train(img):
+        da(img).convert_to_image_format(1.0).resize_image_randomly(1.0, size_range=(299, 303)).crop_picture_randomly(1.0, sizes=(299, 299)).normalize_picture(1.0, value=10.).horizontal_flipping(0.5).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+    @staticmethod
+    def test(img):
+        da(img).convert_to_image_format(1.0).resize_image_randomly(1.0, size_range=(299, 299), interpolation='bilinear').normalize_picture(1.0, value=10.).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+
 class DataAugmentationNormalizeSmall(object):
 
     @staticmethod
@@ -77,6 +90,32 @@ class DataAugmentationNormalizeBig(object):
     @staticmethod
     def test(img):
         da.load_picture(img).resize_image_randomly(1.0, size_range=(128, 128), interpolation='bilinear').normalize_picture(1.0, value=10.).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+
+class DataAugmentationNormalizeBigger(object):
+
+    @staticmethod
+    def train(img):
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(256, 512)).crop_picture_randomly(1.0, sizes=(224, 224)).normalize_picture(1.0, value=10.).horizontal_flipping(0.5).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+    @staticmethod
+    def test(img):
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(384, 384), interpolation='bilinear').normalize_picture(1.0, value=10.).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+
+class DataAugmentationNormalizeHuge(object):
+
+    @staticmethod
+    def train(img):
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(299, 303)).crop_picture_randomly(1.0, sizes=(299, 299)).normalize_picture(1.0, value=10.).horizontal_flipping(0.5).convert_to_chainer_format(1.0)
+        return da.x, da.info
+
+    @staticmethod
+    def test(img):
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(299, 299), interpolation='bilinear').normalize_picture(1.0, value=10.).convert_to_chainer_format(1.0)
         return da.x, da.info
 
 
